1fa494f12b7bf47354e3bd2c74f99e25a8aa1faa
cdadar <gunbanmi@gmail.com>
2018-07-01 21:34:33 +0800

    add example1

---
+++ example1.scm
@@ -0,0 +1,17 @@
+(define (square x) (* x x))
+
+(define (avderage x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x )) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
